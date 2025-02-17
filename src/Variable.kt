c7147f0db4545420f2f06348188d60dfe2a07f79
juhyeon <juhyeon1114@gmail.com>
2025-02-17 21:40:30 +0900

    init

---
+++ src/Variable.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    // 변수 선언
+    val a: Int = 1
+    val b = 1
+
+    // 지연 할당
+    val c: Int
+    c = 3
+
+    // 변수 선언 키워드
+    val d: Int = 1 // Value -> 불변 변수
+    var e: Int = 1 // Variable -> 가변 변수
+
+}
